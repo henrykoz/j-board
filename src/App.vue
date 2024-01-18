31f54b04ddcba2dfa40c02f5b231617420ba0e82
henrykoz <h3nryk.ozog@gmail.com>
2024-01-18 21:58:17 +0000

    name & age test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-
+  <p>{{ name }} - {{ age }}</p>
+  <button @click="changeName('Monika')">change name</button>
+  <button @click="changeAge(21)">change age</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: { }
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Link",
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: string | number) {
+      this.age = age;
+      return age;
+    },
+  },
 });
 </script>
 
-<style>
- 
-</style>
+<style></style>
