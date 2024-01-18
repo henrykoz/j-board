7f77aaccf9ff4af789a723a67ba71245e4640270
henrykoz <h3nryk.ozog@gmail.com>
2024-01-18 23:12:29 +0000

    board template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="app">
-    <p>{{ jobs[1].title }} {{ jobs[1].location }}</p>
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import JobList from "./components/JobsList.vue";
 import Job from "./assets/types/Job";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
 
   setup() {
     const jobs = ref<Job[]>([
       { title: "farm worker", location: "Swindon", salary: "30000", id: "1" },
-      { title: "farm worker", location: "Dudley", salary: "30000", id: "2" },
-      { title: "farm worker", location: "Dudley", salary: "30000", id: "3" },
-      { title: "farm worker", location: "Dudley", salary: "30000", id: "4" },
-      { title: "farm worker", location: "Dudley", salary: "30000", id: "5" },
+      { title: "technican", location: "Dudley", salary: "24000", id: "2" },
+      { title: "stone mason", location: "Rome", salary: "28000", id: "3" },
+      { title: "engineer", location: "Birmingham", salary: "32000", id: "4" },
+      {
+        title: "factory worker",
+        location: "Hinckley",
+        salary: "16000",
+        id: "5",
+      },
     ]);
     return { jobs };
   },
 });
 </script>
 
-<style></style>
+<style scoped>
+
+</style>
